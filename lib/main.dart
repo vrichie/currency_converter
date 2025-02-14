f53db09ff90d328007249bf7aedec0c065efea52
dev.ritchie <dev.ritchiek@gmail.com>
2025-02-14 18:15:37 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,27 +6,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
-/*
-
-/// Stateless vs Stateful Widgets:
-///
-/// - Stateless Widgets:
-///   - These widgets do not have any mutable state.
-///   - Once created, they cannot change their appearance or behavior.
-///   - They are immutable and are typically used for static content.
-///   - Examples include `Text`, `Icon`, and `Image`.
-///
-/// - Stateful Widgets:
-///   - These widgets have mutable state that can change over time.
-///   - They can rebuild themselves when their internal state changes.
-///   - They are used for dynamic content that can change in response to user interactions or other events.
-///   - Examples include `Checkbox`, `Slider`, and `Form`.
-///
-/// In summary, use Stateless Widgets for static content and Stateful Widgets for dynamic content that needs to change.
-
-
-*/
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
